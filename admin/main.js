2a2f92c872899920146832907e0e28cd792ff6f8
Pepe Doval <pepellou@gmail.com>
2020-03-18 15:34:40 +0100

    WIP

--- admin/main.js
+++ admin/main.js
@@ -16,6 +16,22 @@ var get_link = function(tweet) {
     return tweet.url != undefined ? tweet.url : 'https://twitter.com/' + tweet.nick + '/status/' + tweet.id;
 };
 
+var create_map = function() {
+    var map = L.map('new-modal-map', {
+        minZoom: 3,
+        maxZoom: 15
+    }).setView([39.62,-4.25], 7);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoicGVwZWxsb3UiLCJhIjoiY2s3cXV4c3llMDdidzNmcGxybDNwczJmcCJ9.ygIiC_xyqMwtvghc_8qIhA'
+    }).addTo(map);
+};
+
 $(function() {
     init_firebase();
 
@@ -77,6 +93,7 @@ $(function() {
                 }
             } else if (action == 'new') {
                 $('#new-modal').modal();
+                create_map();
             }
         });
     });
