1484e5f2c69a29ad362913ce4ac7b18696b31e26
Pepe Doval <pepellou@gmail.com>
2020-03-25 23:29:32 +0100

    Page has boundaries

--- src/KB_Tree.js
+++ src/KB_Tree.js
@@ -25,6 +25,7 @@ KB_Tree.Page = function(options) {
     this.type = KB_Tree.Page.PointsPage;
     this.children = [];
     this._pagesize = (options && options.pagesize) ? options.pagesize : 2;
+    this.boundaries = null;
 
     this.insert = function(point) {
         if (this.children.length == this._pagesize) {
@@ -33,20 +34,44 @@ KB_Tree.Page = function(options) {
         } else {
             this.children.push(point);
         }
+        this._updateBoundaries(point);
+    };
+
+    this._updateBoundaries = function(point) {
+        if (this.boundaries == null) {
+            this.boundaries = {
+                x: [ point.x, point.x ],
+                y: [ point.y, point.y ]
+            };
+        } else {
+            if (point.x < this.boundaries.x[0]) {
+                this.boundaries.x[0] = point.x;
+            }
+            if (point.x > this.boundaries.x[1]) {
+                this.boundaries.x[1] = point.x;
+            }
+            if (point.y < this.boundaries.y[0]) {
+                this.boundaries.y[0] = point.y;
+            }
+            if (point.y > this.boundaries.y[1]) {
+                this.boundaries.y[1] = point.y;
+            }
+        }
     };
 
     this.splitAndAdd = function(point) {
         let newChildren = [
-            new KB_Tree.Page(this._pagesize),
-            new KB_Tree.Page(this._pagesize)
+            new KB_Tree.Page({ pagesize: this._pagesize }),
+            new KB_Tree.Page({ pagesize: this._pagesize })
         ];
         for (var i = 0; i < this._pagesize / 2; i++) {
             newChildren[0].insert(this.children[i]);
         }
-        for (var i = this._pagesize / 2; i < this.children.length; i++) {
+        for (; i < this.children.length; i++) {
             newChildren[1].insert(this.children[i]);
         }
         newChildren[1].insert(point);
+
         this.children = newChildren;
     };
 
--- test/KB_Tree.spec.js
+++ test/KB_Tree.spec.js
@@ -22,10 +22,10 @@ describe('KB_Tree', function() {
         it('should create a region when count > page size', function() {
             for (var i = 0; i < PAGE_SIZE; i++) {
                 expect(theTree.root.type).to.be(KB_Tree.Page.PointsPage);
-                theTree.insert(aPoint);
+                theTree.insert({ x: i, y: i });
             }
 
-            theTree.insert(aPoint);
+            theTree.insert({ x: 99, y: 99 });
 
             expect(theTree.root.type).to.be(KB_Tree.Page.RegionPage);
             expect(theTree.root.children.length).to.be(2);
@@ -68,28 +68,46 @@ describe('KB_Tree.Page', function() {
             expect(thePage.children[0]).to.be(aPoint);
         });
 
+        it('should update the boundaries of the page', function() {
+            var thePage = new KB_Tree.Page();
+            var aPoint = { x: 1, y: 2 };
+            var anotherPoint = { x: 4, y: 12 };
+
+            expect(thePage.boundaries).to.eql(null);
+
+            thePage.insert(aPoint);
+            expect(thePage.boundaries).to.eql({
+                x: [ 1, 1 ],
+                y: [ 2, 2 ]
+            });
+
+            thePage.insert(anotherPoint);
+            expect(thePage.boundaries).to.eql({
+                x: [ 1, 4 ],
+                y: [ 2, 12 ]
+            });
+        });
+
         describe('when the page size is reached', function() {
 
             let thePage;
-            let aPoint;
 
             beforeEach(function() {
                 thePage = new KB_Tree.Page({ pagesize: 2 });
-                aPoint = { x: 1, y: 2 };
-                thePage.insert(aPoint);
-                thePage.insert(aPoint);
+                thePage.insert({ x: 1, y: 2 });
+                thePage.insert({ x: 0, y: 4 });
             });
 
             it('should convert from PointsPage into RegionPage', function() {
                 expect(thePage.type).to.be(KB_Tree.Page.PointsPage);
 
-                thePage.insert(aPoint);
+                thePage.insert({ x: 9, y: 9 });
 
                 expect(thePage.type).to.be(KB_Tree.Page.RegionPage);
             });
 
             it('should have 2 PointsPage as children', function() {
-                thePage.insert(aPoint);
+                thePage.insert({ x: 9, y: 9 });
 
                 expect(thePage.children[0].type).to.be(KB_Tree.Page.PointsPage);
                 expect(thePage.children[1].type).to.be(KB_Tree.Page.PointsPage);
@@ -97,6 +115,25 @@ describe('KB_Tree.Page', function() {
                     thePage.children[0].children.length +
                     thePage.children[1].children.length
                 ).to.be(thePage._pagesize + 1);
+            });
+
+            it('should properly update its boundaries and its children\'s boundaries', function() {
+                thePage.insert({ x: 9, y: 9 });
+
+                expect(thePage.boundaries).to.eql({
+                    x: [ 0, 9 ],
+                    y: [ 2, 9 ]
+                });
+
+                expect(thePage.children[0].boundaries).to.eql({
+                    x: [ 1, 1 ],
+                    y: [ 2, 2 ]
+                });
+
+                expect(thePage.children[1].boundaries).to.eql({
+                    x: [ 0, 9 ],
+                    y: [ 4, 9 ]
+                });
             });
 
         });
