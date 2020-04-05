bcd814f0351e917065ce9463da09d1705f0a23df
Pepe Doval <pepellou@gmail.com>
2020-04-05 04:11:56 +0200

    Update credentials for firebase

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyCNCfCoMnJMx7ncX6An3zCQc4TWsK60300",
-    authDomain: "ayuda-alimentos-coronavirus.firebaseapp.com",
-    databaseURL: "https://ayuda-alimentos-coronavirus.firebaseio.com",
-    projectId: "ayuda-alimentos-coronavirus",
-    storageBucket: "ayuda-alimentos-coronavirus.appspot.com",
-    messagingSenderId: "622677547690",
-    appId: "1:622677547690:web:0a04757a7c6ab63dacced3"
+    apiKey: "AIzaSyAF4LB7Q1ILsZVp6Rgm5pLhPZmkLbaTGME",
+    authDomain: "brasilcontracoronavirus.firebaseapp.com",
+    databaseURL: "https://brasilcontracoronavirus.firebaseio.com",
+    projectId: "brasilcontracoronavirus",
+    storageBucket: "brasilcontracoronavirus.appspot.com",
+    messagingSenderId: "988399451620",
+    appId: "1:988399451620:web:726850b8874fde00cd1bf1",
+    measurementId: "G-XB1D3S92XV"
 };
-
